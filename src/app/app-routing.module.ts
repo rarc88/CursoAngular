222af9931c62cc03ae7e51586171c10649e199de
rarc88 <rarc88@gmail.com>
2019-04-09 09:05:17 -0500

    Directivas en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { ClickCounterDirective } from './directives/clickCounter.directive';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightDirective,
-    ClickCounterDirective
+    ClickCounterDirective,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
